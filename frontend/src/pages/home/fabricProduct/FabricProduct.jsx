cc0ad457be4ddb7a9afa4f4b018eaafb54414377
Ankit BagheL <ak9664773@gmail.com>
2023-12-07 16:40:22 +0530

    -

--- frontend/src/pages/home/fabricProduct/FabricProduct.jsx
+++ frontend/src/pages/home/fabricProduct/FabricProduct.jsx
@@ -11,6 +11,8 @@ function FabricProduct() {
             <FabricProductTile img={"https://fabriclore.com/cdn/shop/products/R-RYSP01718-HEAD_2d1b0a4a-550f-4a74-aa1c-4873a5154a86.jpg?v=1692341788&width=1020"} title={"Rayon Cloth"} price={"₹23,023"} />
             <FabricProductTile img={"https://5.imimg.com/data5/FL/FG/HW/SELLER-29519557/plain-blue-velvet-fabric.jpg"} title={"Velvet Cloth"} price={"₹43,943"} />
             <FabricProductTile img={"https://images.jdmagicbox.com/quickquotes/images_main/plain-woollen-fabric-377648881-crtow.jpg"} title={"Wool Cloth"} price={"₹10,232"} />
+            <FabricProductTile img={"https://5.imimg.com/data5/FL/FG/HW/SELLER-29519557/plain-blue-velvet-fabric.jpg"} title={"Velvet Cloth"} price={"₹43,943"} />
+            <FabricProductTile img={"https://5.imimg.com/data5/FL/FG/HW/SELLER-29519557/plain-blue-velvet-fabric.jpg"} title={"Velvet Cloth"} price={"₹43,943"} />
         </div>
     )
 }
